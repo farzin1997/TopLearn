d432938ba8d8122cd4fdee2a4a8203ad6b8595af
farzin1997 <ffadaeian97@gmail.com>
2022-02-03 18:38:18 +0330

    USeformikInLoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,62 +1,56 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  Pressable,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Image, TextInput} from 'react-native';
+import {Formik} from 'formik';
 import Icon from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 import CustomButton from '../components/CustomButton';
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [click, setClick] = useState(true);
-
+  
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require('../assets/image/logo.png')} />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <TextInput
-          placeholder="ایمیل کاربری"
-          keyboardType="email-address"
-          autoComplete="email"
-          autoCorrect={false}
-          style={styles.textInput}
-          onChangeText={text => setEmail(text)}
-        />
-        <View style={styles.iconContainer}>
-          <Icon name={'email'} size={25} color={'royalblue'} />
-        </View>
-      </View>
-      <View style={{flexDirection: 'row'}}>
-        <TextInput
-          placeholder="رمز عبور"
-          autoComplete="password"
-          autoCorrect={false}
-          style={styles.textInput}
-          secureTextEntry={click ? true : false}
-          onChangeText={text => setPassword(text)}
-        />
-        <Pressable
-          style={styles.iconContainer}
-          onPress={() => setClick(!click)}>
-          <Icon name={'lock'} size={25} color={'royalblue'} />
-        </Pressable>
-      </View>
-      <View style={styles.button}>
-        <CustomButton
-          title={'ورود کاربر'}
-          onPress={() => console.log('email:', email, 'password:', password)}
-        />
-      </View>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleSubmit}) => (
+          <>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <TextInput
+                placeholder="ایمیل کاربری"
+                keyboardType="email-address"
+                autoComplete="email"
+                autoCorrect={false}
+                style={styles.textInput}
+                onChangeText={handleChange('email')}
+              />
+              <View style={styles.iconContainer}>
+                <Icon name={'email'} size={25} color={'royalblue'} />
+              </View>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <TextInput
+                placeholder="رمز عبور"
+                autoComplete="password"
+                autoCorrect={false}
+                style={styles.textInput}
+                secureTextEntry
+                onChangeText={handleChange('password')}
+              />
+              <View style={styles.iconContainer}>
+                <Icon name={'lock'} size={25} color={'royalblue'} />
+              </View>
+            </View>
+            <View style={styles.button}>
+              <CustomButton title={'ورود کاربر'} onPress={handleSubmit} />
+            </View>
+          </>
+        )}
+      </Formik>
     </View>
   );
 };
