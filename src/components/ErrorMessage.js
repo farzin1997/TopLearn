fd23ef9d748e941ba3413babb00ef5bf8e671755
farzin1997 <ffadaeian97@gmail.com>
2022-02-03 20:56:29 +0330

    yup & component errorMessage

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const ErrorMessage = ({error}) => {
+  if (!error) return null;
+  return <Text style={styles.error}>{error}</Text>;
+};
+
+export default ErrorMessage;
+
+const styles = StyleSheet.create({
+  error: {
+    color: 'red',
+    fontFamily: 'ih',
+  },
+});
