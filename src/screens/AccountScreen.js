8193b2d64ffda9dd02d8e838b7a35aeea7054ba7
farzin1997 <ffadaeian97@gmail.com>
2022-03-11 11:54:40 +0330

    camera

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Image, Text, TouchableHighlight} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import Icon from '../components/shared/Icon';
@@ -7,22 +7,61 @@ import Screen from '../components/shared/Screen';
 import {useSelector} from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {StackActions} from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
 
 const AccountScreen = ({navigation}) => {
+  const [getImage, setImage] = useState(null);
+
+  useEffect(() => {
+    const loadingImage = async () => {
+      const imageUri = await AsyncStorage.getItem('Image');
+      if (imageUri !== null) {
+        setImage(imageUri);
+      }
+    };
+    loadingImage();
+  }, []);
+
   const user = useSelector(state => state.user);
-  
+
   const handleLogout = async () => {
     await AsyncStorage.removeItem('token');
     await AsyncStorage.removeItem('userId');
     navigation.dispatch(StackActions.replace('Welcome'));
   };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+        if (!result.cancelled) {
+            await AsyncStorage.setItem("Image", result.uri);
+            setImage(result.uri);
+        }
+    };
+
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require('../assets/image/logo.png')}
-        />
+        <TouchableOpacity onPress={pickImage}>
+                    {getImage ? (
+                        <Image
+                            source={{ uri: getImage }}
+                            style={styles.image}
+                        />
+                    ) : (
+                        <Image
+                            style={styles.image}
+                            source={require('../assets/image/favicon.png')}
+                        />
+                    )}
+                </TouchableOpacity>
         <View style={styles.details}>
           <Text style={styles.title}>{user.fullname}</Text>
           <Text style={styles.subTitle}>{user.email}</Text>
